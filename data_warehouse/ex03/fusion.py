20bdbc37fca01d378f242fb214d2853195715071
lamizana <lamizana1987@yahoo.fr>
2025-01-28 17:12:10 +0100

    update module data_warehouse

--- data_warehouse/ex03/fusion.py
+++ data_warehouse/ex03/fusion.py
@@ -7,3 +7,41 @@
 #                                       https://github.com/Lamizana #
 #####################################################################
 # Importations de fonctions externes :
+
+
+#####################################################################
+# Variables globales :
+
+
+
+#####################################################################
+# Definitions locales de fonctions :
+def color(texte: str, couleur="37", style="0") -> str:
+    """
+    Applique une couleur et un style à un texte.
+    - couleur : Code couleur de 30 a 47 (ex: '31' pour rouge).
+    - style : Style du texte de 0 a 5(ex: '1' pour gras).
+    """
+    return f"\033[{style};{couleur}m{texte}\033[0m"
+
+
+# ---------------------------------------------------------------- #
+def main() -> int:
+    """
+    Fonction programme principal.
+    """
+
+    print(color("\n\t-------------------------------", 35, 1))
+    print(color("\tLANCEMENT DU PROGRAMME Fusion!!", 35, 1))
+    print(color("\t-------------------------------", 35, 1), "\n")
+
+    print(color("\n\t-------------------------", 35, 1))
+    print(color("\tFERMETURE DU PROGRAMME !!", 35, 1))
+    print(color("\t-------------------------", 35, 1), "\n")
+    return (0)
+
+
+#####################################################################
+# Programme principal :
+if __name__ == "__main__":
+    main()
